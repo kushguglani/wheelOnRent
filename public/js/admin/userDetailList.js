0c7747b9d038ac2b3c9ed9d56f8b6c66a316e079
DzinnApp <dzinnapp@gmail.com>
2019-02-24 19:50:13 +0530

    drop city remove from user

--- public/js/admin/userDetailList.js
+++ public/js/admin/userDetailList.js
@@ -6,7 +6,7 @@ window.onload = function () {
 	// deleteVehicle(2);
 	axios.get('/fetchAllAgents').then(res => {
 		console.log(res);
-		let tdataArray = res.data.map((curr, i) => `<tr class='clearfix'><td>${++i}</td><td> ${curr.agent_name}</td><td>${curr.agent_mobile}</td><td>${curr.agent_address} ${curr.agent_city} ${curr.agent_zip_code} ${curr.agent_state}</td><td>${curr.agent_company_name}</td><td>${curr.agent_registration_type}</td><td>${curr.agent_bookin_city.join(', ')}</td>}</td></tr>`);
+		let tdataArray = res.data.map((curr, i) => `<tr class='clearfix'><td>${++i}</td><td> ${curr.agent_name}</td><td>${curr.agent_mobile}</td><td>${curr.agent_address} ${curr.agent_city} ${curr.agent_zip_code} ${curr.agent_state}</td><td>${curr.agent_company_name}</td><td>${curr.agent_registration_type}</td><td>${curr.agent_service_city.join(', ')}</td></td></tr>`);
 		let tdata = tdataArray.join('');
 		document.querySelector('.userDetails').innerHTML = tdata;
 	})
